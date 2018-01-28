64d2f6b877f2946225f8d7f5985bab13ed894b7e
Liz Zitnik <lizzitnik4@gmail.com>
2018-01-28 11:59:58 -0800

    final commit

--- test/test-fizz-buzzer.js
+++ test/test-fizz-buzzer.js
@@ -3,28 +3,36 @@ const expect = require('chai').expect;
 const fizzBuzzer = require('../fizzBuzzer');
 
 describe('fizzBuzzer', function() {
-	it('should return a string with certain inputs', function() {
-		const normalCases = [
-		{num: 30, expected: 'fizzbuzz'},
-		{num: 10, expected: 'buzz'},
-		{num: 9, expected: 'fizz'}
-		];
+	it('should return "fizz-buzz" for multiples of 15', function() {
+    	[15, 30, 45].forEach(function(input) {
+      		expect(fizzBuzzer(input)).to.equal('fizz-buzz');
+    	});
+  	});
 
-		normalCases.forEach(function(input) {
-			const answer = fizzBuzzer(input.num);
-			expect(answer).to.equal(input.expected);
-		});
+  	it('should return "fizz" for multiples of 3', function() {
+    	[3, 6, 9, 12].forEach(function(input) {
+      		expect(fizzBuzzer(input)).to.equal('fizz');
+    	});
+  	});
+
+  	it('should return "buzz" for multiples of 5', function() {
+    	[5, 10, 20].forEach(function(input) {
+      		expect(fizzBuzzer(input)).to.equal('buzz');
+    	});
+  	});
+
+	it('should return number if not mult of 3 or 5', function() {
+	    [1, 2, 4, 7].forEach(function(input) {
+	      	expect(fizzBuzzer(input)).to.equal(input);
+	    });
 	});
 
-	it('should raise error if num is not a number', function() {
-		const badInputs = [
-		[num: false],
-		[num: 'a'],
-		];
-
-		badInputs.forEach(function(input) {
-			const answer = fizzBuzzer(input.num);
-			expect(answer).to.throw(Error);
-		});
+  	it('should produce error if input isn\'t number', function() {
+    	const badInputs = [true, false, 'cat', function() {}, [1, 2, 3]];
+    	badInputs.forEach(function(input) {
+      		expect(function() {
+          		fizzBuzzer(input);
+			}).to.throw(Error);		
+  		});
 	});
 });
